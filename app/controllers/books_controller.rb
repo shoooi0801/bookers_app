689cc673ee9a84cf627a21cabaef2876aa7ea8cc
shoi <138567675+shoooi0801@users.noreply.github.com>
2023-07-28 10:39:49 +0000

    課題2 新規登録機能の作成途中

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,17 @@
 class BooksController < ApplicationController
+
   def new
+    @book= Book.new
+  end
+
+   # 以下を追加
+  def create
+    # １.&2. データを受け取り新規登録するためのインスタンス作成
+    book = Book.new(book_params)
+    # 3. データをデータベースに保存するためのsaveメソッド実行
+    book.save
+    # 4. トップ画面へリダイレクト
+    redirect_to '/top'
   end
 
   def index
@@ -9,5 +21,11 @@ class BooksController < ApplicationController
   end
 
   def edit
+  end
+
+  private
+  # ストロングパラメータ
+  def book_params
+    params.require(:book).permit(:title, :body)
   end
 end
