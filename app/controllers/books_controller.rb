c41d785bc25fa93b65451430d9a0a7108aa2ca86
shoi <138567675+shoooi0801@users.noreply.github.com>
2023-07-29 16:22:46 +0000

    課題２　途中

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,17 +1,15 @@
 class BooksController < ApplicationController
-
   def new
-    @book= Book.new
+    @book = Book.new
   end
 
-   # 以下を追加
   def create
     # １.&2. データを受け取り新規登録するためのインスタンス作成
     book = Book.new(book_params)
     # 3. データをデータベースに保存するためのsaveメソッド実行
     book.save
     # 4. トップ画面へリダイレクト
-    redirect_to book_path(book.id)
+    redirect_to '/top'
   end
 
   def index
@@ -39,6 +37,7 @@ class BooksController < ApplicationController
   end
 
   private
+  # ストロングパラメータ
   def book_params
     params.require(:book).permit(:title, :body)
   end
