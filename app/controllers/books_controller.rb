8b56567adda3a0b03e1c127578375e5d594375ff
shoi <138567675+shoooi0801@users.noreply.github.com>
2023-08-06 08:18:05 +0000

    課題２提出

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,10 +4,9 @@ class BooksController < ApplicationController
   end
 
   def create
-      @book = Book.new(book_params)
+    @book = Book.new(book_params)
     if @book.save
-      flash[:notice] = "Book was successfully created."
-      redirect_to book_path(@book)
+      redirect_to book_path(@book.id), notice: 'Book was successfully created.'
     else
       @books = Book.all
       render :index
@@ -28,24 +27,23 @@ class BooksController < ApplicationController
   end
 
   def update
-      @book = Book.find(params[:id])
+
+    @book = Book.find(params[:id])
     if @book.update(book_params)
-        flash[:notice] = "Book was successfully updated."
-        redirect_to book_path(@book)
+      redirect_to book_path(@book.id), notice: 'Book was successfully updated.'
     else
-       @books = Book.all
-       render :edit
+      render :edit
     end
   end
 
   def destroy
-      @book = Book.find(params[:id])
-      @book.destroy
-      redirect_to books_path
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path, alert: 'Book was successfully destroyed.'
   end
 
   private
   def book_params
-    params.require(:book).permit(:title, :body)
+    params.require(:book).permit(:title, :body) 
   end
 end
